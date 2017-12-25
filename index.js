e70cdf6f1a5be1397196eb19f7d0860729feb7ec
宋正旭 <2944434952@qq.com>
2017-12-25 18:39:55 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import { AppRegistry } from 'react-native';
+// import App from './src/App';
+// import HellWorldApp from './src/01.HelloWorld';
+import Bananas from './src/02.Bananas';
+
+// AppRegistry.registerComponent('AwesomeProject', () => App);
+// AppRegistry.registerComponent('AwesomeProject', () => HellWorldApp);
+AppRegistry.registerComponent('AwesomeProject', () => Bananas);
